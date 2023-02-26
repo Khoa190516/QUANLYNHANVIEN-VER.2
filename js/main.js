990f98c8f3a1e7591a5a519ed1e2258cc9145c94
Huỵnh The Khoa <lilo190516@gmail.com>
2023-02-27 00:21:56 +0700

    add view trip, nha xe, xe, nhan vien

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function validate() {
     if (username == "admin" && password == "123456") {
         swal({
             title: "",
-            text: "Xin chào Võ Trường",
+            text: "Xin chào Minh",
             icon: "success",
             close: true,
             button: false,
