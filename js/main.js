b86e4bc9c378ae1ab89048263b40325757a10c0f
Huỵnh The Khoa <lilo190516@gmail.com>
2023-03-20 00:42:50 +0700

    update ui, fix api reload bug

--- js/main.js
+++ js/main.js
@@ -1,48 +1,49 @@
 /* =========================================== */
 /* =========================================== */
 
-function login(){
+function login() {
   var password = document.getElementById("password-field").value;
   var otp = $("#otp").text();
+
   if (password == otp) {
-        swal({
-            title: "",
-            text: "Xin chào Minh",
-            icon: "success",
-            close: true,
-            button: false,
-          });
-        window.location = "custommer.html";
-        return true;
-    }
+    swal({
+      title: "",
+      text: "Xin chào Minh",
+      icon: "success",
+      close: true,
+      button: false,
+    });
+    window.location = "doc/index.html";
+    return true;
+  }
 }
 
 function validate() {
 
-    var loginUrl = 'https://localhost:7026/api/v1/accounts/login';
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password-field").value;
-    //Đặt 1 Admin ảo để đăng nhập quản trị
-    console.log(username);
-    var otp ='';
-    var token = '';
+  var loginUrl = 'https://localhost:44305/api/v1/accounts/login';
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password-field").value;
+  //Đặt 1 Admin ảo để đăng nhập quản trị
+  console.log(username);
+  var otp = '';
+  var token = '';
 
-    // if(otp!='a'){
-    //   if(otp==password){
-    //     window.location = "custommer.html";
-    //     return true;
-    //   }
-    // }
-    
-    fetch(loginUrl, {
+  // if(otp!='a'){
+  //   if(otp==password){
+  //     window.location = "custommer.html";
+  //     return true;
+  //   }
+  // }
+
+  fetch(loginUrl, {
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+
     },
     body: JSON.stringify({ "email": username })
-    })
+  })
     .then(response => response.json())
     .then(response => {
       console.log(JSON.stringify(response));
@@ -50,84 +51,92 @@ function validate() {
       console.log(result);
       otp = response.otp
       token = response.token;
-      localStorage.setItem('token',token);
+      localStorage.setItem('token', token);
       console.log(otp);
       console.log(token);
       $("#otp").text(otp);
+      swal({
+        title: "Thông báo",
+
+        text: "Đã gửi mã OTP đến email,\n hãy nhập mã OTP để đăng nhập",
+        buttons: ["Hủy bỏ", "Đồng ý"],
+      })
+      $(".email-login-form").hide();
+      $(".email-login-form").text = "";
+      $(".otp-form").show();
     });
 
-    $(".email-login-form").hide();
-    $(".otp-form").show();
 
-    // if (username == "admin" && password == "123456") {
-    //     swal({
-    //         title: "",
-    //         text: "Xin chào Minh",
-    //         icon: "success",
-    //         close: true,
-    //         button: false,
-    //       });
-    //     window.location = "custommer.html";
-    //     return true;
-       
-    // }
-    // //Nếu không nhập gì mà nhấn đăng nhập thì sẽ báo lỗi
-    // if (username == "" && password == "") {
-    //     swal({
-    //         title: "",
-    //         text: "Bạn chưa điền đầy đủ thông tin đăng nhập...",
-    //         icon: "error",
-    //         close: true,
-    //         button: "Thử lại",
-    //       });
-         
-    //     return false;
-       
-    // }
-    // //Nếu không nhập mật khẩu mà đúng tài khoản 
-    // if (username == "admin" && password == "") {
-    //     swal({
-    //         title: "",
-    //         text: "Bạn chưa nhập mật khẩu...",
-    //         icon: "warning",
-    //         close: true,
-    //         button: "Thử lại",
-    //       });
-    //     return false;
-    // }
-    // //Nếu không nhập tài khoản sẽ báo lỗi
-    // if (username == null || username == "") {
-    //     swal({
-    //         title: "",
-    //         text: "Tài khoản đang để trống...",
-    //         icon: "warning",
-    //         close: true,
-    //         button: "Thử lại",
-    //       });
-    //     return false;
-    // }
-    // //Nếu không nhập mật khẩu sẽ báo lỗi
-    // if (password == null || password == "") {
-    //     swal({
-    //         title: "",
-    //         text: "Mật khẩu đang để trống...",
-    //         icon: "warning",
-    //         close: true,
-    //         button: "Thử lại",
-    //       });
-    //     return false;
-    // }
-    // //Nếu trống toàn bộ thì báo lỗi
-    // else {
-    //     swal({
-    //         title: "",
-    //         text: "Sai thông tin đăng nhập hãy kiểm tra lại...",
-    //         icon: "error",
-    //         close: true,
-    //         button: "Thử lại",
-    //       });
-    //     return true;
-    // };
+
+  // if (username == "admin" && password == "123456") {
+  //     swal({
+  //         title: "",
+  //         text: "Xin chào Minh",
+  //         icon: "success",
+  //         close: true,
+  //         button: false,
+  //       });
+  //     window.location = "custommer.html";
+  //     return true;
+
+  // }
+  // //Nếu không nhập gì mà nhấn đăng nhập thì sẽ báo lỗi
+  // if (username == "" && password == "") {
+  //     swal({
+  //         title: "",
+  //         text: "Bạn chưa điền đầy đủ thông tin đăng nhập...",
+  //         icon: "error",
+  //         close: true,
+  //         button: "Thử lại",
+  //       });
+
+  //     return false;
+
+  // }
+  // //Nếu không nhập mật khẩu mà đúng tài khoản 
+  // if (username == "admin" && password == "") {
+  //     swal({
+  //         title: "",
+  //         text: "Bạn chưa nhập mật khẩu...",
+  //         icon: "warning",
+  //         close: true,
+  //         button: "Thử lại",
+  //       });
+  //     return false;
+  // }
+  // //Nếu không nhập tài khoản sẽ báo lỗi
+  // if (username == null || username == "") {
+  //     swal({
+  //         title: "",
+  //         text: "Tài khoản đang để trống...",
+  //         icon: "warning",
+  //         close: true,
+  //         button: "Thử lại",
+  //       });
+  //     return false;
+  // }
+  // //Nếu không nhập mật khẩu sẽ báo lỗi
+  // if (password == null || password == "") {
+  //     swal({
+  //         title: "",
+  //         text: "Mật khẩu đang để trống...",
+  //         icon: "warning",
+  //         close: true,
+  //         button: "Thử lại",
+  //       });
+  //     return false;
+  // }
+  // //Nếu trống toàn bộ thì báo lỗi
+  // else {
+  //     swal({
+  //         title: "",
+  //         text: "Sai thông tin đăng nhập hãy kiểm tra lại...",
+  //         icon: "error",
+  //         close: true,
+  //         button: "Thử lại",
+  //       });
+  //     return true;
+  // };
 }
 
 /*  PHẦN NỘI DUNG KHÔI PHỤC MẬT KHẨU   */
@@ -141,30 +150,31 @@ function validate() {
 //        return false;
 //    }
 //}
-function RegexEmail(emailInputBox) {
-    var emailStr = document.getElementById(emailInputBox).value;
-    var emailRegexStr = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    var isvalid = emailRegexStr.test(emailStr);
-    if (!isvalid) {
-        swal({
-            title: "",
-            text: "Bạn vui lòng nhập đúng định dạng email...",
-            icon: "error",
-            close: true,
-            button: "Thử lại",
-          });
-        
-        emailInputBox.focus;
-    } else {
-        swal({
-            title: "",
-            text: "Chúng tôi vừa gửi cho bạn email hướng dẫn đặt lại mật khẩu vào địa chỉ cho bạn",
-            icon: "success",
-            close: true,
-            button: "Đóng",
-          });
-        emailInputBox.focus;
-        window.location = "#";
 
-    }
+function RegexEmail(emailInputBox) {
+  var emailStr = document.getElementById(emailInputBox).value;
+  var emailRegexStr = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  var isvalid = emailRegexStr.test(emailStr);
+  if (!isvalid) {
+    swal({
+      title: "",
+      text: "Bạn vui lòng nhập đúng định dạng email...",
+      icon: "error",
+      close: true,
+      button: "Thử lại",
+    });
+
+    emailInputBox.focus;
+  } else {
+    swal({
+      title: "",
+      text: "Chúng tôi vừa gửi cho bạn email hướng dẫn đặt lại mật khẩu vào địa chỉ cho bạn",
+      icon: "success",
+      close: true,
+      button: "Đóng",
+    });
+    emailInputBox.focus;
+    window.location = "#";
+
+  }
 }
